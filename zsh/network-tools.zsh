d91cf84f249a4cd52091c13dd60110d01329d441
hophacker <j@paiyou.co>
2022-05-06 10:37:23 +0800

    feat: add host_ip which shows your host ip

--- zsh/network-tools.zsh
+++ zsh/network-tools.zsh
@@ -1,5 +1,7 @@
 alias intercept_request_hosts="sudo tcpdump -i any -A -vv -s 0 |  grep -e 'Host:'"
-
+host_ip() {
+  ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $7}'
+}
 test_port() {
   nc -z localhost $1
 }
