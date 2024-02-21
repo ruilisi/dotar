6f4f7925bc8e00364d7da4106e6dd7cb9ec9ba2b
hophacker <j@paiyou.co>
2024-02-21 14:48:39 +0800

    fix: upgrade commitlint init script

--- zsh/commitlint.zsh
+++ zsh/commitlint.zsh
@@ -1,6 +1,6 @@
 commitlint_init() {
-  yarn add husky @commitlint/{config-conventional,cli} -D -W
+  yarn add husky @commitlint/{config-conventional,cli} -D
   echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
-  yarn husky install
-  yarn husky add .husky/commit-msg 'yarn commitlint --edit $1'
+  yarn husky init
+  echo 'yarn commitlint --edit $1' > .husky/commit-msg
 }
